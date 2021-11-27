3c83df8f372a44c6e4aa22f42e9d30c02b9d07a2
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-11-27 16:11:46 -0500

    Add mobile menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
-import { Container, Flex, HStack, Link } from "@chakra-ui/layout"
+import { useDisclosure } from "@chakra-ui/hooks"
+import { Container, Flex } from "@chakra-ui/layout"
 import { Logo } from "@components/Logo"
-import { LINKEDIN_LINK, NAV_ITEMS } from "@constants"
 import { createTransition } from "@utils"
 import { useRouter } from "next/dist/client/router"
 import NextLink from "next/link"
-import { FaLinkedinIn } from "react-icons/fa"
+import { DesktopMenu } from "./DesktopMenu"
+import { MobileMenu } from "./MobileMenu"
 
 const Navbar: React.FC = () => {
 	const router = useRouter()
+	const disclosure = useDisclosure()
 
 	return (
 		<Flex
@@ -23,69 +25,20 @@ const Navbar: React.FC = () => {
 				<Flex justify="space-between" align="center">
 					<NextLink href="/" passHref>
 						<Logo
+							onClick={disclosure.onClose}
 							h="45px"
 							w="auto"
 							cursor="pointer"
+							position="relative"
+							zIndex={99999}
 							transition={createTransition("transform")}
 							_hover={{ transform: "scale(1.05)" }}
 							_focus={{ transform: "scale(1.05)", outline: "none" }}
 							_active={{ transform: "scale(0.95)" }}
 						/>
 					</NextLink>
-					<HStack spacing={4} as="nav">
-						{NAV_ITEMS.map((navItem) => (
-							<NextLink
-								href={navItem.path}
-								passHref
-								key={`${navItem.title}-nav-item`}
-							>
-								<Link
-									title={navItem.title}
-									isExternal={navItem.isExternal}
-									textDecoration="none !important"
-									position="relative"
-									_hover={{
-										_after: {
-											width: "50%",
-										},
-									}}
-									_focus={{
-										outline: "none",
-										_after: {
-											width: "50%",
-										},
-									}}
-									_active={{ transform: "scale(0.975)" }}
-									_after={{
-										content: '""',
-										position: "absolute",
-										bottom: "-4px",
-										left: "50%",
-										height: "1px",
-										width:
-											router.pathname === navItem.path ? "110% !important" : 0,
-										transform: "translateX(-50%)",
-										bg: "black",
-										transition: createTransition("width"),
-									}}
-								>
-									{navItem.title}
-								</Link>
-							</NextLink>
-						))}
-						<NextLink href={LINKEDIN_LINK} passHref>
-							<Link
-								title="LinkedIn"
-								isExternal
-								transition={createTransition("transform")}
-								_hover={{ transform: "scale(1.05)" }}
-								_focus={{ transform: "scale(1.05)", outline: "none" }}
-								_active={{ transform: "scale(0.95)" }}
-							>
-								<FaLinkedinIn fontSize="1.25rem" />
-							</Link>
-						</NextLink>
-					</HStack>
+					<DesktopMenu activePath={router.pathname} />
+					<MobileMenu activePath={router.pathname} disclosure={disclosure} />
 				</Flex>
 			</Container>
 		</Flex>
