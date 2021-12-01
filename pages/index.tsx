f0c3f6761075da0736af9c8aff2f7ac41d054f70
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-11-30 19:01:57 -0500

    Add remaining home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@chakra-ui/button"
 import { Box, Container, Flex, Text } from "@chakra-ui/layout"
 import { SITE_NAME } from "@constants"
+import ArticleImage from "@images/home/article.jpg"
 import LandingImage from "@images/home/landing.jpg"
+import { fade } from "@utils"
 import type { NextPage } from "next"
 import { NextSeo } from "next-seo"
 import NextImage from "next/image"
@@ -11,11 +13,10 @@ const HomePage: NextPage = () => {
 	return (
 		<>
 			<NextSeo titleTemplate="%s" />
-			<Flex justify="center" align="center" w="full" px={8} py={8}>
+			<Box w="full" px={8} py={12}>
 				<Container maxW="container.lg" p={0}>
 					<Flex
 						w="full"
-						pb={8}
 						align="center"
 						flexDirection={{ base: "column", md: "row" }}
 					>
@@ -62,7 +63,108 @@ const HomePage: NextPage = () => {
 						</Box>
 					</Flex>
 				</Container>
-			</Flex>
+			</Box>
+			<Box
+				minH={{ base: "200px", md: "250px" }}
+				w="full"
+				position="relative"
+				overflow="hidden"
+			>
+				<Box
+					as="video"
+					autoPlay
+					loop
+					muted
+					w="full"
+					position="absolute"
+					zIndex={100}
+					left={0}
+					top="72.5%"
+					transform="translateY(-72.5%)"
+				>
+					<source src="/images/home/quote-background.mp4" type="video/mp4" />
+				</Box>
+				<Flex
+					justify="center"
+					align="center"
+					flexDirection="column"
+					textAlign="center"
+					px={8}
+					w="full"
+					h="full"
+					position="absolute"
+					bg={fade("#fff", 0.4)}
+					zIndex={101}
+				>
+					<Text
+						as="h3"
+						fontSize={{ base: "4xl", md: "5xl", lg: "6xl" }}
+						lineHeight={1.2}
+						fontWeight="bold"
+						mb={4}
+					>
+						&ldquo;Change is the only constant.&ldquo;
+					</Text>
+					<Text fontSize="xl" fontStyle="italic">
+						&mdash; Heraclitus
+					</Text>
+				</Flex>
+			</Box>
+			<Box w="full" px={8} py={12}>
+				<Container
+					maxW="container.lg"
+					position="relative"
+					zIndex={199}
+					p={0}
+					pb={{ base: 24, sm: 12, md: 0 }}
+				>
+					<Box w={{ base: "90%", lg: "70%" }}>
+						<Flex w="full" justify="center" position="relative" zIndex={99}>
+							<NextImage
+								src={ArticleImage}
+								width={1600}
+								height={757}
+								placeholder="blur"
+								alt={SITE_NAME}
+							/>
+						</Flex>
+					</Box>
+					<Box
+						w={{ base: "90%", md: "45%" }}
+						bg="brand.100"
+						position="absolute"
+						zIndex={200}
+						right={0}
+						bottom={{ base: 0, md: "inherit" }}
+						top={{ md: "50%" }}
+						transform={{ md: "translateY(-50%)" }}
+						p={{ base: 4, sm: 6, lg: 8 }}
+					>
+						<Text
+							as="h4"
+							fontSize={{ base: "lg", md: "xl", lg: "2xl" }}
+							fontWeight="bold"
+							lineHeight={1.2}
+							mb={6}
+						>
+							Growing Up South Asian American
+						</Text>
+						<NextLink
+							href="https://www.southasianproductions.com/blog/if-im-proud-of-who-i-am-then-others-will-be-too"
+							passHref
+						>
+							<Button
+								as="a"
+								title="Growing Up South Asian American"
+								textDecoration="none !important"
+								variant="filled"
+							>
+								Read My Blog Post.
+							</Button>
+						</NextLink>
+					</Box>
+				</Container>
+			</Box>
 		</>
 	)
 }
