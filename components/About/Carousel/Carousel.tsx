f7edade75ec3e44b667c48741354bca31db9ff8f
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-12-08 11:12:07 -0500

    Update carousel

--- components/About/Carousel/Carousel.tsx
+++ components/About/Carousel/Carousel.tsx
@@ -1,11 +1,11 @@
 import { IconButton } from "@chakra-ui/button"
-import { Box, Flex } from "@chakra-ui/layout"
-import { createTransition } from "@utils"
+import { Box, Container } from "@chakra-ui/layout"
 import NextImage from "next/image"
-import { useMemo, useState } from "react"
-import AliceCarousel from "react-alice-carousel"
-import "react-alice-carousel/lib/alice-carousel.css"
+import { useMemo, useRef } from "react"
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs"
+import Slider, { Settings } from "react-slick"
+import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css"
 
 export type CarouselImage = {
 	src: string
@@ -23,123 +23,91 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
 		() =>
 			images.map(({ src, width, height, blurDataUrl }) => (
 				<Box
-					h="300px"
-					w={`${(width * 300) / height}px`}
 					pointerEvents="none"
 					userSelect="none"
-					position="relative"
-					sx={{
-						"& div": {
-							display: "inline-block !important",
-							"& .carousel-image": {
-								display: "inline-block !important",
-							},
-						},
-					}}
 					key={`${src}-carousel-image`}
 				>
-					<NextImage
-						src={src}
-						layout="fill"
-						objectFit="contain"
-						objectPosition="center center"
-						placeholder="blur"
-						blurDataURL={blurDataUrl}
-						className="carousel-image"
-					/>
+					<Box h="300px" w={`${(width * 300) / height}px`} position="relative">
+						<NextImage
+							src={src}
+							layout="fill"
+							objectFit="contain"
+							objectPosition="center center"
+							placeholder="blur"
+							blurDataURL={blurDataUrl}
+						/>
+					</Box>
 				</Box>
 			)),
 		[images]
 	)
 
-	const [activeIndex, setActiveIndex] = useState<number>(0)
-	const slidePrev = () => setActiveIndex(activeIndex - 1)
-	const slideNext = () => setActiveIndex(activeIndex + 1)
-	const syncActiveIndex = ({ item }: { item: number }) => setActiveIndex(item)
+	const sliderRef = useRef<Slider | null>(null)
+
+	const settings: Settings = {
+		infinite: true,
+		centerMode: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		swipe: true,
+		swipeToSlide: true,
+		variableWidth: true,
+		arrows: false,
+		adaptiveHeight: true,
+	}
 
 	return (
-		<Flex
+		<Container
+			maxW="min(calc(100vw - 32px - 32px), var(--chakra-sizes-container-lg))"
+			h="300px"
 			w="full"
-			minW={0}
-			maxW="min(calc(100vw - 32px - 32px - 15px), var(--chakra-sizes-container-lg))"
-			overflowX="hidden"
-			sx={{
-				"& .alice-carousel__wrapper": {
-					h: "300px",
-					cursor: "grab",
-					overflow: "hidden",
-					position: "relative",
-					w: "full",
-					paddingLeft: "33% !important",
-					paddingRight: "33% !important",
-					_active: { cursor: "grabbing" },
-				},
-			}}
+			position="relative"
+			zIndex={99}
+			p={0}
 		>
 			<IconButton
-				onClick={slidePrev}
 				icon={<BsArrowLeft />}
+				onClick={() => sliderRef.current?.slickPrev()}
 				aria-label="Previous image"
-				display="flex"
-				justifyContent="flex-start"
-				alignItems="center"
-				flex="0 0 40px"
+				position="absolute"
+				zIndex={100}
+				left={0}
+				top="50%"
+				transform="translate(-50%, -50%)"
+				borderRadius="50%"
 				color="black"
-				variant="unstyled"
-				h="300px"
-				borderRadius={0}
-				opacity={0.8}
-				textAlign="center"
-				transition={createTransition(["opacity", "font-size"])}
-				_hover={{
-					opacity: 1,
-					fontSize: "lg",
-				}}
-				_focus={{
-					boxShadow: "none",
-					outline: "none",
-					opacity: 1,
-				}}
+				bg="white"
+				w="32px"
+				h="32px"
+				size="sm"
 			/>
-			<Box flex="1 1 auto" minW={0}>
-				<AliceCarousel
-					items={carouselItems}
-					autoWidth
-					infinite
-					mouseTracking
-					touchTracking
-					disableDotsControls
-					disableButtonsControls
-					onSlideChanged={syncActiveIndex}
-					activeIndex={activeIndex}
-				/>
+			<Box
+				w="full"
+				cursor="grab"
+				overflowX="hidden"
+				_active={{ cursor: "grabbing" }}
+			>
+				<Slider ref={sliderRef} {...settings}>
+					{carouselItems}
+				</Slider>
 			</Box>
 			<IconButton
-				onClick={slideNext}
 				icon={<BsArrowRight />}
+				onClick={() => sliderRef.current?.slickNext()}
 				aria-label="Next image"
-				display="flex"
-				justifyContent="flex-end"
-				alignItems="center"
-				flex="0 0 40px"
+				position="absolute"
+				zIndex={100}
+				right={0}
+				top="50%"
+				transform="translate(50%, -50%)"
+				borderRadius="50%"
 				color="black"
-				variant="unstyled"
-				h="300px"
-				borderRadius={0}
-				opacity={0.8}
-				transition={createTransition(["opacity", "font-size"])}
-				textAlign="right"
-				_hover={{
-					opacity: 1,
-					fontSize: "lg",
-				}}
-				_focus={{
-					boxShadow: "none",
-					outline: "none",
-					opacity: 1,
-				}}
+				bg="white"
+				w="32px"
+				h="32px"
+				size="sm"
 			/>
-		</Flex>
+		</Container>
 	)
 }
 
