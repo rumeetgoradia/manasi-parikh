fdcbbe0170f2c476a018c85a37d6796aa20df963
Rumeet Goradia <rumeet.goradia@gmail.com>
2023-06-03 14:24:40 -0400

    Initialize with create-t3-app

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,22 @@
-// @ts-check
-import withPlaiceholder from "@plaiceholder/next";
-
 /**
- * @type {import('next').NextConfig}
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
  */
-const config = {
-	transpilePackages: ["@plaiceholder/ui"],
-	images: {
-		domains: [],
-	},
-	async redirects() {
-		return [
-			{
-				source: "/resume",
-				destination: "/Manasi_Parikh_Resume.pdf",
-				permanent: true,
-			},
-		];
-	},
-};
+await import("./src/env.mjs");
 
-export default withPlaiceholder(config);
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default config;
